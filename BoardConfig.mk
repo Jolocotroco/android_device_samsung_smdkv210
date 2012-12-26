2cadedb0e0ef3fbef0cee446fc55b2861b626331
jol <jolocotrocomail@gmail.com>
2012-12-26 18:47:04 +0100

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,106 @@
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# BoardConfig.mk
+#
+# Product-specific compile-time definitions.
+#
+BOARD_USES_GENERIC_AUDIO := false
+
+# ARMv7-A Cortex-A8 architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_PREBUILT_KERNEL := device/samsung/smdkv210/zImage
+TARGET_PROVIDES_INIT_TARGET_RC := true
+
+TARGET_BOARD_PLATFORM := s5pc110
+TARGET_BOARD_PLATFORM_GPU := POWERVR_SGX540_120
+TARGET_BOOTLOADER_BOARD_NAME := smdkv210
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/smdkv210/bluetooth
+
+BOARD_USES_HGL := true
+DEFAULT_FB_NUM := 2
+
+# skia
+BOARD_USE_SKIA_LCDTEXT := true
+# SkTextBox for libtvout
+BOARD_USES_SKTEXTBOX := true
+
+# Camera
+USE_CAMERA_STUB := false
+BOARD_CAMERA_LIBRARIES := camera.smdkv210
+
+# Wi-Fi
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_atheros
+BOARD_HOSTAPD_DRIVER := AR6000
+BOARD_HOSTAPD_PRIVATE_LIB :=
+BOARD_WLAN_DEVICE := ar6002
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/ar6000.ko"
+WIFI_DRIVER_MODULE_NAME := "ar6000"
+WIFI_DRIVER_MODULE_ARG := ""
+WIFI_DRIVER_LOADER_DELAY := 3000000
+
+# 3G
+BOARD_MOBILEDATA_INTERFACE_NAME := "ppp0"
+
+# Sensors
+BOARD_USES_GENERIC_INVENSENSE := false
+
+# Vold
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun%d/file"
+
+# OpenGL stuff
+BOARD_EGL_CFG := device/samsung/smdkv210/prebuilt/lib/egl/egl.cfg
+USE_OPENGL_RENDERER := true
+
+# adb has root
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=0
+ADDITIONAL_DEFAULT_PROPERTIES += persist.service.adb.enable=1
+
+# Boot Animation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+#TARGET_USE_HWDECODING_TVOUT := true
+
+#TARGET_SEC_OMX_BIG_MMAP_BUFFER_SIZE := true
+
+# TARGET_DISABLE_TRIPLE_BUFFERING can be used to disable triple buffering
+# on per target basis. On crespo it is possible to do so in theory
+# to save memory, however, there are currently some limitations in the
+# OpenGL ES driver that in conjunction with disable triple-buffering
+# would hurt performance significantly (see b/6016711)
+TARGET_DISABLE_TRIPLE_BUFFERING := false
+
+BOARD_ALLOW_EGL_HIBERNATION := true
+
+# hwcomposer: custom vsync ioctl
+BOARD_CUSTOM_VSYNC_IOCTL := true
+
+
---
+++ device.mk
@@ -0,0 +1,204 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# These is the hardware-specific overlay, which points to the location
+# of hardware-specific resource overrides, typically the frameworks and
+# application settings that are stored in resourced.
+DEVICE_PACKAGE_OVERLAYS := device/samsung/smdkv210/overlay
+
+# Bootanimation
+TARGET_SCREEN_WIDTH := 800
+TARGET_SCREEN_HEIGHT := 480
+
+# Screen size is "large", density is "hdpi"
+PRODUCT_AAPT_CONFIG := large hdpi
+PRODUCT_LOCALES += hdpi
+
+# Init files
+PRODUCT_COPY_FILES := \
+	device/samsung/smdkv210/init.smdkv210.rc:root/init.smdkv210.rc \
+	device/samsung/smdkv210/init.smdkv210.usb.rc:root/init.smdkv210.usb.rc \
+	device/samsung/smdkv210/fstab.smdkv210:root/fstab.smdkv210 \
+	device/samsung/smdkv210/ueventd.smdkv210.rc:root/ueventd.smdkv210.rc
+
+# These are the hardware-specific configuration files
+PRODUCT_COPY_FILES += \
+	device/samsung/smdkv210/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
+	device/samsung/smdkv210/prebuilt/lib/egl/egl.cfg:system/lib/egl/egl.cfg
+
+# Prebuilt kl and kcm keymaps + idc files.
+PRODUCT_COPY_FILES += \
+	device/samsung/smdkv210/prebuilt/usr/idc/ft5x0x_ts.idc:system/usr/idc/ft5x0x_ts.idc \
+	device/samsung/smdkv210/prebuilt/usr/idc/S5P_TouchScreen.idc:system/usr/idc/S5P_TouchScreen.idc \
+	device/samsung/smdkv210/prebuilt/usr/keychars/ft5x0x_ts.kcm:system/usr/keychars/ft5x0x_ts.kcm \
+	device/samsung/smdkv210/prebuilt/usr/keychars/s3c-button.kcm:system/usr/keychars/s3c-button.kcm \
+	device/samsung/smdkv210/prebuilt/usr/keychars/S5P_TouchScreen.kcm:system/usr/keychars/S5P_TouchScreen.kcm \
+	device/samsung/smdkv210/prebuilt/usr/keylayout/ft5x0x_ts.kl:system/usr/keylayout/ft5x0x_ts.kl \
+	device/samsung/smdkv210/prebuilt/usr/keylayout/s3c-button.kl:system/usr/keylayout/s3c-button.kl \
+	device/samsung/smdkv210/prebuilt/usr/keylayout/S5P_TouchScreen.kl:system/usr/keylayout/S5P_TouchScreen.kl
+
+# Generated kcm keymaps
+PRODUCT_PACKAGES := \
+ 	s3c-button.kcm \
+ 	ft5x0x_ts.kcm \
+	S5P_TouchScreen.kcm
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	make_ext4fs \
+	setup_fs
+
+# Wi-Fi
+PRODUCT_COPY_FILES += \
+	device/samsung/smdkv210/prebuilt/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+	device/samsung/smdkv210/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	device/samsung/smdkv210/prebuilt/etc/wifi/hostapd.conf:system/etc/wifi/hostapd.conf \
+	device/samsung/smdkv210/prebuilt/lib/modules/ar6000.ko:system/lib/modules/ar6000.ko \
+	device/samsung/smdkv210/prebuilt/wifi/data.patch.hw2_0.bin:system/wifi/data.patch.hw2_0.bin \
+	device/samsung/smdkv210/prebuilt/wifi/athwlan.bin.z77:system/wifi/athwlan.bin.z77 \
+	device/samsung/smdkv210/prebuilt/wifi/fakeBoardData_AR6002.bin:system/wifi/fakeBoardData_AR6002.bin
+
+# MFC Firmware
+PRODUCT_COPY_FILES += \
+        device/samsung/smdkv210/prebuilt/vendor/firmware/samsung_mfc_fw.bin:system/vendor/firmware/samsung_mfc_fw.bin
+
+# PowerVR libs
+PRODUCT_COPY_FILES += \
+	device/samsung/smdkv210/prebuilt/vendor/bin/pvrsrvinit:system/vendor/bin/pvrsrvinit \
+	device/samsung/smdkv210/prebuilt/vendor/lib/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/libpvr2d.so:system/vendor/lib/libpvr2d.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/libsrv_init.so:system/vendor/lib/libsrv_init.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/libusc.so:system/vendor/lib/libusc.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/libsrv_um.so:system/vendor/lib/libsrv_um.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/libIMGegl.so:system/vendor/lib/libIMGegl.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+	device/samsung/smdkv210/prebuilt/vendor/lib/hw/gralloc.s5pc110.so:system/vendor/lib/hw/gralloc.s5pc110.so
+
+# Lights
+PRODUCT_PACKAGES += \
+	lights.smdkv210
+
+# Audio
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+	audio.usb.default \
+	audio.primary.smdkv210 \
+	audio_policy.smdkv210
+
+PRODUCT_COPY_FILES += \
+	device/samsung/smdkv210/libaudio/audio_policy.conf:system/etc/audio_policy.conf
+
+# Camera
+PRODUCT_PACKAGES += \
+	camera.smdkv210\
+	libs3cjpeg 
+
+# These are the OpenMAX IL configuration files
+PRODUCT_COPY_FILES += \
+	device/samsung/smdkv210/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
+	device/samsung/smdkv210/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
+	device/samsung/smdkv210/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml
+
+# These are the OpenMAX IL modules
+PRODUCT_PACKAGES += \
+	libSEC_OMX_Core \
+	libOMX.SEC.AVC.Decoder \
+	libOMX.SEC.M4V.Decoder \
+	libOMX.SEC.M4V.Encoder \
+	libOMX.SEC.AVC.Encoder
+
+# Libs
+PRODUCT_PACKAGES += \
+	libcamera \
+	hwcomposer.smdkv210 \
+	libstagefrighthw \
+	librs_jni
+
+# Usb accessory
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory
+
+# Misc other modules
+PRODUCT_PACKAGES += \
+	sensors.smdkv210
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+
+# The OpenGL ES API level that is natively supported by this device.
+# This is a 16.16 fixed point number
+PRODUCT_PROPERTY_OVERRIDES := \
+ 	ro.opengles.version=131072
+
+# sysctl tweaks
+PRODUCT_COPY_FILES += \
+	device/samsung/smdkv210/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf \
+	device/samsung/smdkv210/prebuilt/etc/init.d/01sysctl:system/etc/init.d/01sysctl
+
+# dpi
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=120
+
+# These are the hardware-specific settings that are stored in system properties.
+# Note that the only such settings should be the ones that are too low-level to
+# be reachable from resources or other mechanisms.
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.interface=wlan0 \
+	wifi.supplicant_scan_interval=45
+
+# enable Google-specific location features,
+# like NetworkLocationProvider and LocationCollector
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.com.google.locationfeatures=1 \
+	ro.com.google.networklocation=1
+
+# Extended JNI checks
+# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
+# before they have a chance to cause problems.
+# Default=true for development builds, set by android buildsystem.
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.kernel.android.checkjni=0 \
+	dalvik.vm.checkjni=false
+
+# Override /proc/sys/vm/dirty_ratio on UMS
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.vold.umsdirtyratio=20
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Dalvik heap limits.
+include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mass_storage
+
+# Set product characteristic to tablet, needed for some ui elements
+PRODUCT_CHARACTERISTICS := tablet
